fb6bee82d46693606c91986885bfc973dcbbf96d
Bryan Woods <bryan@dropio.com>
2011-01-04 20:42:34 -0500

    Adding header comments with problem description

--- problem3/solution.rb
+++ problem3/solution.rb
@@ -1,3 +1,6 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
 def largest_prime_factor_of(limit, factor=2)
   while limit > 1
     limit % factor == 0 ? limit /= factor : factor += 1
