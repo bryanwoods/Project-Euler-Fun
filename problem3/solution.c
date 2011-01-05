03c87958248f0146250d09e8e28358a97b3f02b1
Bryan Woods <bryan@dropio.com>
2011-01-04 21:00:49 -0500

    Adding C solution for problem 3

---
+++ problem3/solution.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* 
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143 ?
+*/
+
+void largest_prime_factor_of(long int limit, int factor)
+{
+    while (limit > 1) {
+        (limit % factor == 0) ? (limit /= factor) : (factor += 1);
+    }
+    printf("%d\n", factor);
+}
+
+main ()
+{
+    largest_prime_factor_of(600851475143, 2);
+}
