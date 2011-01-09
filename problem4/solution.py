6b7880dc0e401fbb8b6b12be5d20a582513ed584
Bryan Woods <bryan@dropio.com>
2011-01-09 12:22:43 -0500

    Tabs to spaces in python solution for problem 4

--- problem4/solution.py
+++ problem4/solution.py
@@ -7,16 +7,16 @@
 largest = 0
 
 def reverse(s):
-	return s[::-1]	
+  return s[::-1]	
 
 def palindrome(i):
-	if str(i) == reverse(str(i)):
-		return True
+  if str(i) == reverse(str(i)):
+    return True
 
 for fact1 in range(100, 1000):
-	for fact2 in range(fact1, 1000):
-		product = fact1 * fact2
-		if palindrome(product) and product > largest:
-			largest = product
+  for fact2 in range(fact1, 1000):
+    product = fact1 * fact2
+    if palindrome(product) and product > largest:
+      largest = product
 
 print largest
