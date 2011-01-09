48aabaed37e32e0451c1ebfed1ff2479853c60d6
Bryan Woods <bryan@dropio.com>
2011-01-09 12:19:44 -0500

    Refactoring Python problem 4 solution

--- problem4/solution.py
+++ problem4/solution.py
@@ -6,9 +6,17 @@
 
 largest = 0
 
+def reverse(s):
+	return s[::-1]	
+
+def palindrome(i):
+	if str(i) == reverse(str(i)):
+		return True
+
 for fact1 in range(100, 1000):
 	for fact2 in range(fact1, 1000):
 		product = fact1 * fact2
-		if str(product) == str(product)[::-1] and product > largest:
+		if palindrome(product) and product > largest:
 			largest = product
+
 print largest
