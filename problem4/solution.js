d17793730e2257220f6239eef18d595be88552c5
Bryan Woods <bryan@dropio.com>
2011-01-09 12:01:25 -0500

    Adding problem description header comments to CoffeeScript, JavaScript, and Python problem 4 solutions

--- problem4/solution.js
+++ problem4/solution.js
@@ -1,3 +1,9 @@
+//#########################################################################################
+//# A palindromic number reads the same both ways.                                        #
+//# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99. #
+//# Find the largest palindrome made from the product of two 3-digit numbers.             #
+//#########################################################################################
+
 (function() {
   var fact1, fact2, largest, palindrome, product, reverse;
   largest = 0;
--- problem4/solution.py
+++ problem4/solution.py
@@ -1,3 +1,9 @@
+#########################################################################################
+# A palindromic number reads the same both ways.                                        #
+# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99. #
+# Find the largest palindrome made from the product of two 3-digit numbers.             #
+#########################################################################################
+
 largest = 0
 
 for fact1 in range(100, 1000):
