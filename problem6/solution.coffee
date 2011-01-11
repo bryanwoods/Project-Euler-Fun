e90868098269c8886ba96d59ddca302ba6aac72e
Bryan Woods <bryan@dropio.com>
2011-01-10 23:02:25 -0500

    Adding header comments to problem 6 solutions

--- problem6/solution.coffee
+++ problem6/solution.coffee
@@ -1,7 +1,22 @@
+###
+
+The sum of the squares of the first ten natural numbers is,
+12 + 22 + ... + 102 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten
+Natural numbers and the square of the sum is 3025  385 = 2640.
+
+Find the difference between the sum of the squares of the first one
+Hundred natural numbers and the square of the sum.
+
+###
+
 [squares, sum] = [0, 0]
 
 for i in [1..100]
-  squares += i
-  sum     += i
+  if squares += i then sum += i
 
 console.log (sum * sum) - squares
--- problem6/solution.js
+++ problem6/solution.js
@@ -1,10 +1,24 @@
 (function() {
-  var i, squares, sum;
-  squares = 0;
-  sum = 0;
+  /*
+
+  The sum of the squares of the first ten natural numbers is,
+  12 + 22 + ... + 102 = 385
+
+  The square of the sum of the first ten natural numbers is,
+  (1 + 2 + ... + 10)2 = 552 = 3025
+
+  Hence the difference between the sum of the squares of the first ten
+  Natural numbers and the square of the sum is 3025  385 = 2640.
+
+  Find the difference between the sum of the squares of the first one
+  Hundred natural numbers and the square of the sum.
+
+  */  var i, squares, sum, _ref;
+  _ref = [0, 0], squares = _ref[0], sum = _ref[1];
   for (i = 1; i <= 100; i++) {
-    squares += i;
-    sum += i;
+    if (squares += i) {
+      sum += i;
+    }
   }
   console.log((sum * sum) - squares);
 }).call(this);
