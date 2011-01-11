b2ff2561b5f519bdcb447b1338e6be79393c24f0
Bryan Woods <bryan@dropio.com>
2011-01-10 23:19:29 -0500

    Cleaning up solution.c for problem 6

--- problem6/solution.c
+++ problem6/solution.c
@@ -2,9 +2,11 @@
 
 void sum_of_squares_difference()
 {
-    int squares = 0;
-    int sum     = 0;
-    int i       = 0;
+    int squares;
+    int sum;
+    int i;
+
+		squares, sum, i = 0, 0, 0;
 
     for (i; i <= 100; i++) {
         squares += i;
