5590df8a78e02b128cb4d9f547dddfee16bc1ebc
Bryan Woods <bryanwoods4e@gmail.com>
2011-08-07 14:43:45 -0400

    Problem 1:   * Adds Clojure Solution   * Reduces embarrassing Ruby solution to one line

--- problem1/solution.rb
+++ problem1/solution.rb
@@ -1,8 +1,1 @@
-def sum_of_multiples_in_range(range, mult1, mult2, ints=[])
-  range.first.upto(range.last).each do |i|
-    ints.push(i) if i % mult1 == 0 || i % mult2 == 0
-  end
-  puts ints.inject(0) { |sum, value| sum += value }
-end
-
-sum_of_multiples_in_range(0..999, 3, 5)
+puts (1..999).select { |i| i % 3 == 0 || i % 5 == 0 }.reduce(:+)
