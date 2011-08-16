686d8c2b8984c29c0afb37269e896bed18e4146a
Bryan Woods <bryanwoods4e@gmail.com>
2011-08-15 23:07:21 -0400

    Fixes weird spacing

--- problem9/solution.rb
+++ problem9/solution.rb
@@ -1,13 +1,13 @@
 limit = 1000
 
 1.upto(limit) do |a|
-	1.upto(limit) do |b|
-		c = limit - b - a
+  1.upto(limit) do |b|
+    c = limit - b - a
 
-		if a ** 2 + b ** 2 == c ** 2
-			@result = a * b * c
-		end
-	end
+    if a ** 2 + b ** 2 == c ** 2
+      @result = a * b * c
+    end
+  end
 end
 
 puts @result
