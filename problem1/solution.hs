06de86aedf36d0173e9a2ecbb81dccbb355e76e8
Bryan Woods <pair+bryan@howaboutwe.com>
2012-01-08 15:17:05 -0500

    Uses Haskell's built-in sum function for problem 1 solution

--- problem1/solution.hs
+++ problem1/solution.hs
@@ -1,4 +1,4 @@
 multipleOfThreeOrFive :: (Int -> Bool)
-multipleOfThreeOrFive x = x `mod` 3 == 0 || x `mod` 5 == 0
+multipleOfThreeOrFive x = (x `mod` 3 == 0 || x `mod` 5 == 0)
 
-main = print (foldl (+) 0 (filter multipleOfThreeOrFive [1..999]))
+main = print (sum $ filter multipleOfThreeOrFive [1..999])
